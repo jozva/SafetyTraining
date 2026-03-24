16adfd110cc5d2dd74d7445fb71c8f845bfe5c1b
jozva <kingsonjozva@gmail.com>
2026-03-24 11:58:26 +0530

    fix advertisement bars

--- src/components/landingPage/AdvertisementBar.jsx
+++ src/components/landingPage/AdvertisementBar.jsx
@@ -4,17 +4,17 @@ function AdvertisementBar() {
   return (
     <section className="advertisement-bar">
 
-      <div className="advertisement-container-main">
+      <div className="advertisement-container">
 
         <p className="ad-highlight">
           SUNDAY CLASSES AVAILABLE ! ENROLL NOW
         </p>
 
-        <h2 className="ad-title">
+        <h2 className="ad-title-add">
           Workplace Health and Safety Training Courses in Sydney
         </h2>
 
-        <p className="ad-links">
+        <p className="ad-links-add">
           Construction Short Courses • Earthmoving • HRW Training & 
           <span> VOC's for All Courses</span>
         </p>
